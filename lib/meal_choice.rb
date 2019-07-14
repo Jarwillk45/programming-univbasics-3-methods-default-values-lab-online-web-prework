db834e9b4e78bf788b5eddaa53f50818c5ff02a2
looping-messaging-1494 <wilkins.jarred@outlook.com>
2019-07-14 22:02:22 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,7 +6,7 @@ def meal_choice(protein="meat",veg1,veg2)
   puts"What a nutritious meal!"
   puts"A plate of #{protein} with #{veg1} and #{veg2}."
 end  
-if meal_choice(protein="meat",veg1,veg2)
+if meal_choice(protein,veg1,veg2)
   puts"What a nutritioius meal!"
   puts"A plate of #{protein} with #{veg1} and #{veg2}."
 end
